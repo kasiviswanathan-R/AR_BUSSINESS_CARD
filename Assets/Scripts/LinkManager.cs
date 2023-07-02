2a9ee412736c9f52ba616c0cb495e50fab69c611
Kasiviswanathan-R <kasiviswanath1258@gmail.com>
2023-07-02 21:05:03 +0530

    f

---
+++ Assets/Scripts/LinkManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    public void openLinkedin(){
+        Application.OpenURL("https://www.linkedin.com/in/kasi-viswanathan-5658b2278");
+    }
+    public void openGithub(){
+        Application.OpenURL("https://github.com/kasiviswanathan-R");
+    }
+    
+}
